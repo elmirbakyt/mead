2bb4adfbd0ab81e26644e23edacfeb22e0a45ba6
elmirbakyt <kalipasemaelmir@gmail.com>
2022-11-27 10:35:34 +0600

    media

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
     pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+      el: ".swiper-pagination",
+      clickable: true,
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
     },
-});
+  });
